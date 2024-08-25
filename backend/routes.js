921a00640e4aa51bfe4f3e2457f497765d3e9162
Vitor Rangel <vitorfdrangel@gmail.com>
2024-08-25 15:06:31 -0300

    Rota de update finalizada

--- backend/routes.js
+++ backend/routes.js
@@ -9,6 +9,7 @@ const {
   getMemories,
   getMemory,
   deleteMemory,
+  updateMemory,
 } = require("./controllers/MemoryController");
 
 router.post(
@@ -31,5 +32,9 @@ router.get("/", (req, res) => getMemories(req, res));
 router.get("/:id", (req, res) => getMemory(req, res));
 
 router.delete("/:id", (req, res) => deleteMemory(req, res));
+
+router.patch("/:id", upload.single("image"), (req, res) =>
+  updateMemory(req, res)
+);
 
 module.exports = router;
