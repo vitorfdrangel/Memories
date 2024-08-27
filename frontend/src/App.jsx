4728d4316390c8e6265fa419038e2916a8aa2611
Vitor Rangel <vitorfdrangel@gmail.com>
2024-08-27 17:10:48 -0300

    Navbar e Footer finalizados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
 
+import { Outlet } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Memories</h1>
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/routes/AddMemory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddMemory = () => {
+  return <div>AddMemory</div>;
+};
+
+export default AddMemory;
