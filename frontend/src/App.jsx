197f728b0c4fa683d7050c1515b0d8959461db44
Vitor Rangel <vitorfdrangel@gmail.com>
2024-08-28 19:51:01 -0300

    Envio de memória finalizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
-import "./App.css";
-
 import { Outlet } from "react-router-dom";
+
+// Toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -9,6 +13,18 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Navbar />
       <div className="container">
         <Outlet />
--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,25 +1,76 @@
+import { useState } from "react";
+
+import axios from "../axios-config";
+
+import { toast } from "react-toastify";
+
+import { useNavigate } from "react-router-dom";
+
 // Styles
 import "./AddMemory.css";
 
 const AddMemory = () => {
+  const [inputs, setInputs] = useState({});
+  const [image, setImage] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handdleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("title", inputs.title);
+    formData.append("description", inputs.description);
+    formData.append("image", image);
+
+    try {
+      const response = await axios.post("/memories", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      toast.success(response.data.msg);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.msg);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setImage(e.target.files[0]);
+    } else {
+      setInputs({ ...inputs, [e.target.name]: e.target.value });
+    }
+  };
+
   return (
     <div className="add-memory-page">
       <h2>Crie uma nova memória</h2>
-      <form>
+      <form onSubmit={handdleSubmit}>
         <label>
           <p>Título</p>
-          <input type="text" placeholder="Defina um título" name="title" />
+          <input
+            type="text"
+            placeholder="Defina um título"
+            name="title"
+            onChange={handleChange}
+          />
         </label>
         <label>
           <p>Descrição</p>
           <textarea
             placeholder="Conte-nos o que aconteceu..."
             name="description"
+            onChange={handleChange}
           ></textarea>
         </label>
         <label>
           <p>Foto:</p>
-          <input type="file" name="image" />
+          <input type="file" name="image" onChange={handleChange} />
         </label>
         <input type="submit" className="btn" value="Enviar" />
       </form>
